e45c289241683dbb5ca91313382809b4214e59d3
pHo9UBenaA <102408484+pHo9UBenaA@users.noreply.github.com>
2024-10-14 14:52:34 +1000

    pnpm run format

--- esbuild.js
+++ esbuild.js
@@ -24,7 +24,7 @@ const copyAssets = copyStaticFiles({
 });
 
 const outdir = `${distDir}`;
-const entryPoints = glob.sync(`${srcDir}/*.ts`)
+const entryPoints = glob.sync(`${srcDir}/*.ts`);
 
 const tsOption = {
 	...optionsStatic,
