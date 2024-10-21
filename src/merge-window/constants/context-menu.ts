a5e43fb5e2383f9ce2b8b43b8ad3ab94b21e92bf
pHo9UBenaA <102408484+pHo9UBenaA@users.noreply.github.com>
2024-10-21 10:49:28 +1000

    Delete unused file

--- src/merge-window/constants/context-menu.ts
+++
@@ -1,9 +0,0 @@
-const ContextMenuIdsKey = ['mergeWindow', 'mergeSecretWindow'] as const;
-
-export const ContextMenuIds = {
-	// TODO
-	[ContextMenuIdsKey[0]]: ContextMenuIdsKey[0],
-	[ContextMenuIdsKey[1]]: ContextMenuIdsKey[1],
-} as const satisfies { [key in (typeof ContextMenuIdsKey)[number]]: key };
-
-export type ContextMenuIds = keyof typeof ContextMenuIds;
