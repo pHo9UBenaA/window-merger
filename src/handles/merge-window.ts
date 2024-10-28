a1363ac782677c4b7806475b5977db4b1e94723f
pHo9UBenaA <102408484+pHo9UBenaA@users.noreply.github.com>
2024-10-28 15:05:28 +1000

    chore

--- src/handles/merge-window.ts
+++ src/handles/merge-window.ts
@@ -1,57 +1,59 @@
 const moveTabs = (tabs: chrome.tabs.Tab[], firstWindowId: number): number => {
-	const tabIds = tabs
-		.map((tab) => tab.id)
-		.flatMap((tabId) => (tabId !== undefined ? [tabId] : []));
+  const tabIds = tabs
+    .map((tab) => tab.id)
+    .flatMap((tabId) => (tabId !== undefined ? [tabId] : []));
 
-	for (const id of tabIds) {
-		chrome.tabs.move(id, {
-			windowId: firstWindowId,
-			index: -1,
-		});
-	}
+  for (const id of tabIds) {
+    chrome.tabs.move(id, {
+      windowId: firstWindowId,
+      index: -1,
+    });
+  }
 
-	const fatalCount = tabs.length - tabIds.length;
-	return fatalCount;
+  const fatalCount = tabs.length - tabIds.length;
+  return fatalCount;
 };
 
 const mergeWindow = (windowIds: number[]) => {
-	if (windowIds.length <= 1) {
-		console.info('There was only one window.');
-		return;
-	}
+  if (windowIds.length <= 1) {
+    console.info("There was only one window.");
+    return;
+  }
 
-	const firstWindowId = windowIds.shift();
-	if (!firstWindowId) {
-		console.error('The first window could not be found.');
-		return;
-	}
+  const firstWindowId = windowIds.shift();
+  if (!firstWindowId) {
+    console.error("The first window could not be found.");
+    return;
+  }
 
-	for (const windowId of windowIds) {
-		chrome.tabs.query({ windowId }, (tabs) => {
-			const fatalCount = moveTabs(tabs, firstWindowId);
-			if (fatalCount) {
-				console.error(`Merge skipped because ${fatalCount} tab IDs could not be found`);
-			}
-		});
-	}
+  for (const windowId of windowIds) {
+    chrome.tabs.query({ windowId }, (tabs) => {
+      const fatalCount = moveTabs(tabs, firstWindowId);
+      if (fatalCount) {
+        console.error(
+          `Merge skipped because ${fatalCount} tab IDs could not be found`
+        );
+      }
+    });
+  }
 };
 
 const handleMergeWindowEvent = () => {
-	chrome.windows.getAll({ populate: true }, (windows) => {
-		const windowIds: number[] = windows
-			.filter((window) => !window.incognito && window.type === 'normal')
-			.flatMap((window) => (window.id !== undefined ? [window.id] : []));
-		mergeWindow(windowIds);
-	});
+  chrome.windows.getAll({ populate: true }, (windows) => {
+    const windowIds: number[] = windows
+      .filter((window) => !window.incognito && window.type === "normal")
+      .flatMap((window) => (window.id !== undefined ? [window.id] : []));
+    mergeWindow(windowIds);
+  });
 };
 
 const handleMergeSecretWindowEvent = () => {
-	chrome.windows.getAll({ populate: true }, (windows) => {
-		const secretWindowIds: number[] = windows
-			.filter((window) => window.incognito && window.type === 'normal')
-			.flatMap((window) => (window.id !== undefined ? [window.id] : []));
-		mergeWindow(secretWindowIds);
-	});
+  chrome.windows.getAll({ populate: true }, (windows) => {
+    const secretWindowIds: number[] = windows
+      .filter((window) => window.incognito && window.type === "normal")
+      .flatMap((window) => (window.id !== undefined ? [window.id] : []));
+    mergeWindow(secretWindowIds);
+  });
 };
 
 export { handleMergeSecretWindowEvent, handleMergeWindowEvent };
