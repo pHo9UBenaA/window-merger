4fad18d823f35ff898eca0a8f7e8467581583cf9
pHo9UBenaA <102408484+pHo9UBenaA@users.noreply.github.com>
2024-12-22 22:19:59 +1000

    Revert "selenium検証"
    This reverts commit 657570cd0fcaa5f6dfe6eb66bc22ecb92118a6bf.

--- tests/e2e/selenium-driver.ts
+++
@@ -1,9 +0,0 @@
-import { Builder } from 'selenium-webdriver';
-import * as chromeDriver from 'selenium-webdriver/chrome';
-
-const options = new chromeDriver.Options();
-options.addArguments('--load-extension=/Users/RH/Documents/git/window-merger-extension/dist');
-
-const driver = await new Builder().forBrowser('chrome').setChromeOptions(options).build();
-
-export { driver };
