ae3b578c9c6fe4bfb06c7b5244b9847425ae6c19
pHo9UBenaA <102408484+pHo9UBenaA@users.noreply.github.com>
2025-01-02 21:56:39 +1000

    test: add unit tests

---
+++ vitest.config.ts
@@ -0,0 +1,14 @@
+import { sync as globSync } from 'glob';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+	test: {
+		setupFiles: globSync('./__tests__/setup-files/*.ts'),
+		include: ['./__tests__/**/*.{spec,test}.{ts,tsx}'],
+		mockReset: true,
+		clearMocks: true,
+
+		// TODO
+		// coverage: {},
+	},
+});
