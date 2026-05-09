ff010eddad66120b648b828e7b7780cab45cf934
pHo9UBenaA <102408484+pHo9UBenaA@users.noreply.github.com>
2026-05-10 04:05:23 +0900

    fix(context-menu): await create/update calls to ensure menus are fully applied before resolving

--- src/adapters/chrome/context-menu.ts
+++ src/adapters/chrome/context-menu.ts
@@ -15,20 +15,20 @@ export type ContextMenuTitles = (typeof ContextMenuTitles)[keyof typeof ContextM
 export const setupContextMenus = async (): Promise<void> => {
 	await chrome.contextMenus.removeAll();
 
-	chrome.contextMenus.create({
+	await chrome.contextMenus.create({
 		id: ContextMenuIds.mergeWindow,
 		title: chrome.i18n.getMessage(ContextMenuTitles.mergeWindow),
 		contexts: ['all'],
 	});
 
-	chrome.contextMenus.create({
+	await chrome.contextMenus.create({
 		id: ContextMenuIds.mergeIncognitoWindow,
 		title: chrome.i18n.getMessage(ContextMenuTitles.mergeIncognitoWindow),
 		contexts: ['all'],
 	});
 
 	const isAllowedIncognitoAccess = await chrome.extension.isAllowedIncognitoAccess();
-	chrome.contextMenus.update(ContextMenuIds.mergeIncognitoWindow, {
+	await chrome.contextMenus.update(ContextMenuIds.mergeIncognitoWindow, {
 		enabled: isAllowedIncognitoAccess,
 	});
 };
